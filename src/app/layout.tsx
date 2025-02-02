edd0f26a59192a7d83f7710bac2612a66fbb784f
mukhriddin <mshakhriyorov8@gmail.com>
2025-02-02 11:55:59 +0500

    +

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import { MainNav } from '@/components/nav';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           enableSystem={false}
           storageKey="speakify-theme"
         >
+          <MainNav />
           {children}
           <Toaster />
         </ThemeProvider>
